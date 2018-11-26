604514d470567d13feac46a58841ec3b2e2c4a61
yn.z007 <>
2018-11-26 10:45:15 +0800

    di inject

--- app/src/main/java/com/mm/app/wallpaper/App.java
+++ app/src/main/java/com/mm/app/wallpaper/App.java
@@ -2,6 +2,7 @@ package com.mm.app.wallpaper;
 
 import com.alipictures.statemanager.loader.StateRepository;
 import com.mm.app.wallpaper.base.BaseApplication;
+import com.mm.app.wallpaper.di.AppInjector;
 import com.mm.app.wallpaper.support.state.ExceptionState;
 import com.mm.app.wallpaper.support.state.LoadingState;
 
@@ -12,6 +13,7 @@ public class App extends BaseApplication {
     public void onCreate() {
         super.onCreate();
         regStateManager();
+        AppInjector.init(this);
     }
 
     private void regStateManager(){
--- app/src/main/java/com/mm/app/wallpaper/data/local/AppDb.java
+++ app/src/main/java/com/mm/app/wallpaper/data/local/AppDb.java
@@ -3,9 +3,11 @@ package com.mm.app.wallpaper.data.local;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
+import com.mm.app.wallpaper.data.model.Find;
 import com.mm.app.wallpaper.data.model.Image;
+import com.mm.app.wallpaper.data.model.Type;
 
-@Database(entities = {Image.class},version = 1,exportSchema = false)
+@Database(entities = {Image.class, Type.class, Find.class}, version = 1, exportSchema = false)
 public abstract class AppDb extends RoomDatabase {
 
     public abstract ImagesDao imagesDao();
